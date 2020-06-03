802469b895d9da8eabfb42fdde84f0cc62fcf314
SX <sx@minetest>
2020-06-03 10:51:45 +0300

    Update POI template

--- sharetool/nodes/poi.lua
+++ sharetool/nodes/poi.lua
@@ -10,11 +10,11 @@
 -- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 -- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
--- shared account name
-local shared_account = 'shared'
-
 -- POI nodes
 local nodes = {}
+
+-- get namespace defined at sharetool init.lua
+local ns = metatool.ns('sharetool')
 
 --luacheck: ignore unused argument node player
 return {
@@ -25,29 +25,19 @@ return {
 		protection_bypass_write = 'ban',
 
 		before_read = function(nodedef, pos, player)
-			if not metatool.before_read(nodedef, pos, player) then
-				-- Player is allowed to operate in area without need to bypass protections
+			if ns:can_bypass(pos, player, 'owner') or metatool.before_read(nodedef, pos, player, true) then
+				-- Player is allowed to bypass protections or operate in area
 				return true
 			end
-			-- Allow bypass protection if owner is shared or tool user
-			local name = player:get_player_name()
-			local meta = minetest.get_meta(pos)
-			local owner = meta:get_string('owner')
-			local shared = meta:get_int('sharetool_shared_node')
-			return name == owner or name == shared_account or shared == 1
+			return false
 		end,
 
 		before_write = function(nodedef, pos, player)
-			if metatool.before_write(nodedef, pos, player) then
-				-- Player is allowed to operate in area without need to bypass protections
+			if ns:can_bypass(pos, player, 'owner') or metatool.before_write(nodedef, pos, player, true) then
+				-- Player is allowed to bypass protections or operate in area
 				return true
 			end
-			-- Allow bypass protection if owner is shared or tool user
-			local name = player:get_player_name()
-			local meta = minetest.get_meta(pos)
-			local owner = meta:get_string('owner')
-			local shared = meta:get_int('sharetool_shared_node')
-			return name == owner or name == shared_account or shared == 1
+			return false
 		end,
 
 		copy = function(node, pos, player)
@@ -58,7 +48,7 @@ return {
 			local name = player:get_player_name()
 
 			-- change ownership and mark as shared node
-			meta:set_int('sharetool_shared_node', 1)
+			ns.mark_shared(meta)
 			meta:set_string("owner", name)
 
 			-- return data required for replicating this tube settings
@@ -75,8 +65,8 @@ return {
 			local meta = minetest.get_meta(pos)
 
 			-- change ownership and mark as shared node
-			meta:set_int('sharetool_shared_node', 1)
-			meta:set_string("owner", shared_account)
+			ns.mark_shared(meta)
+			meta:set_string("owner", ns.shared_account)
 		end,
 	}
 }
