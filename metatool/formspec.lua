8a21ea3b877005a345ce470391b6d17e62310d48
SX <sx@minetest>
2020-06-21 21:01:10 +0300

    #21 Teleport tube waypoints

--- metatool/formspec.lua
+++ metatool/formspec.lua
@@ -1,23 +1,41 @@
 
---local S = metatool.S
+local S = metatool.S
 
 metatool.form = {}
 
 -- container for form handler callback methods
 metatool.form.handlers = {}
 
-local getformname = function(formname)
-	return string.format("metatool:%s", formname)
+local get_formname = function(name)
+	if not name then
+		return
+	end
+	local parts = name:gsub('\\s',''):split(':')
+	if #parts < 2 then
+		return
+	elseif parts[1] == "metatool" then
+		if #parts < 3 then
+			return
+		end
+		return name
+	end
+	return string.format("metatool:%s", name)
 end
 
-local hasform = function(name)
+local has_form = function(name)
 	return type(metatool.form.handlers[name]) == 'table'
 end
+
+-- Temporary storage for form data references
+-- Links are created after(!) form is constructed
+-- Links are destroyed after receiving fields
+local formdata = {}
 
 local global_form_handler_registered
 metatool.form.register_global_handler = function()
 	if not global_form_handler_registered then
 		global_form_handler_registered = true
+		print("metatool.form.register_global_handler Registering global formspec handler for Metatool")
 		minetest.register_on_player_receive_fields(metatool.form.on_receive)
 	end
 end
@@ -32,28 +50,36 @@ metatool.form.on_receive = function(player, formname, fields)
 		return
 	end
 	if metatool.form.handlers[formname].on_receive then
+		local playername = player:get_player_name()
+		local data = formdata[playername] and formdata[playername][formname]
 		-- call actual form handler
-		metatool.form.handlers[formname].on_receive(player, fields)
+		metatool.form.handlers[formname].on_receive(player, fields, data)
+		if fields.quit and formdata[playername] then
+			formdata[playername][formname] = nil
+		end
 	end
 end
 
 metatool.form.on_create = function(player, formname, data)
-	local name = getformname(formname)
-	if not hasform(name) then
-		return
+	if has_form(formname) then
+		-- call actual form handler
+		if metatool.form.handlers[formname].on_create then
+			return metatool.form.handlers[formname].on_create(player, data)
+		end
+		return metatool.form.handlers[formname].formspec
 	end
-	-- call actual form handler
-	if metatool.form.handlers[name].on_create then
-		return metatool.form.handlers[name].on_create(player, data)
-	end
-	return metatool.form.handlers[name].formspec
 end
 
 -- Either on_create for dynamic formspecs or formspec for static formspecs.
 -- on_create will be priorized if both defined
 metatool.form.register_form = function(formname, on_create, on_receive, formspec)
+	local name = get_formname(formname)
+	if not name then
+		print(S("metatool.form.register_form Registration failed, invalid formname: %s", formname))
+		return
+	end
 	metatool.form.register_global_handler()
-	local name = getformname(formname)
+	print(S("metatool.form.register_form Registering form: %s", name))
 	metatool.form.handlers[name] = {
 		formspec = formspec,
 		on_create = on_create,
@@ -62,10 +88,19 @@ metatool.form.register_form = function(formname, on_create, on_receive, formspec
 end
 
 metatool.form.show = function(player, formname, data)
-	local name = getformname(formname)
-	if not hasform(name) then
-		return
+	local name = get_formname(formname)
+	if has_form(name) then
+		-- Construct form
+		local formspec = metatool.form.on_create(player, name, data)
+		if metatool.form.handlers[name].on_receive then
+			-- Store temporary data ref if form input is processed somehow
+			local playername = player:get_player_name()
+			if not formdata[playername] then
+				formdata[playername] = {}
+			end
+			formdata[playername][name] = data
+		end
+		-- Show form to player
+		minetest.show_formspec(player:get_player_name(), name, formspec)
 	end
-	local formspec = metatool.form.on_create(player, formname, data)
-	minetest.show_formspec(player:get_player_name(), formname, formspec)
 end
