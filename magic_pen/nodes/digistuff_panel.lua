f575fcb1a1edd284f7be893ceb40258f6ec329bb
SX <sx@minetest>
2020-11-07 15:59:06 +0200

    (#54) Fix digistuff panel/touchscreen

--- magic_pen/nodes/digistuff_panel.lua
+++ magic_pen/nodes/digistuff_panel.lua
@@ -12,7 +12,6 @@ return {
 		copy = function(node, pos, player)
 			local meta = minetest.get_meta(pos)
 			local nicename = minetest.registered_nodes[node.name].description or node.name
-			local content = get_content[node.name](meta)
 			return {
 				description = ("%s at %s"):format(nicename, minetest.pos_to_string(pos)),
 				content = meta:get("text"),
--- magic_pen/nodes/digistuff_touchscreen.lua
+++ magic_pen/nodes/digistuff_touchscreen.lua
@@ -2,6 +2,16 @@
 -- Register Digistuff Touchscreen for Magic pen
 --
 
+local function add_field(t, f)
+	for k,v in pairs(f) do
+		if type(v) == "table" then
+			add_field(t, v)
+		else
+			table.insert(t, tostring(k).."="..tostring(v))
+		end
+	end
+end
+
 --luacheck: ignore unused argument node player
 return {
 	name = 'touchscreen',
@@ -11,10 +21,16 @@ return {
 		copy = function(node, pos, player)
 			local meta = minetest.get_meta(pos)
 			local data = minetest.deserialize(meta:get_string("data"))
+			local content = {}
+			if type(data) == "table" then
+				for _,field in ipairs(data) do
+					add_field(content, field)
+				end
+			end
 			local nicename = minetest.registered_nodes[node.name].description or node.name
 			return {
 				description = ("%s at %s"):format(nicename, minetest.pos_to_string(pos)),
-				content = type(data) == "table" and table.concat(data, "\n"),
+				content = table.concat(content, "\n"),
 			}
 		end,
 	}
