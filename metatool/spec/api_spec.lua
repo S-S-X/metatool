30500d6321da93c32ff893e25bed7c3967f8289f
SX <sx@minetest>
2020-11-21 08:39:25 +0200

    checkout@v2, "mineunit" detached to mt-mods repo

--- metatool/spec/api_spec.lua
+++ metatool/spec/api_spec.lua
@@ -11,9 +11,9 @@
 --]]
 
 dofile("spec/test_helpers.lua")
-fixture("minetest/core")
-fixture("minetest/player")
-fixture("minetest/protection")
+fixture("mineunit/core")
+fixture("mineunit/player")
+fixture("mineunit/protection")
 fixture("metatool")
 
 require("settings")
